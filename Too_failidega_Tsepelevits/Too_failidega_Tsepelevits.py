7179808427208d1928ffb58f705e88b37197d917
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-02-26 22:25:47 +0200

    Update Too_failidega_Tsepelevits.py

--- Too_failidega_Tsepelevits/Too_failidega_Tsepelevits.py
+++ Too_failidega_Tsepelevits/Too_failidega_Tsepelevits.py
@@ -1,7 +1,7 @@
 from OmaMoodul import *
 laused=[]
 while True:
-    menu=input(1-loeme failist \n2-Salvestame failisse \n3-Sõnade tõlkimine \n4-Vaata sõnastikku5-Parandage viga sõnastikus\n6-Harjutus\n)
+    menu=input('1-loeme failist \n2-Salvestame failisse \n3-Sõnade tõlkimine \n4-Vaata sõnastikku5-Parandage viga sõnastikus\n6-Harjutus\n')
 
     while menu.isdigit()==False:
         menu=input("Kirjuta ainult need numbrid, mis on ")
